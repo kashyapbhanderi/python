d37bb540c7405a8b379c06d5cae064644b099f6c
kashyap bhanderi <148347704+kashyapbhanderi@users.noreply.github.com>
2025-07-15 22:24:52 +0530

    search character from given string

---
+++ 5.py
@@ -0,0 +1,12 @@
+name="harray"
+print(name[0:3])#start from index 0 all the way to index 3
+print(name[-4:-1])
+print(name[1:4])
+print(name[:4])# it same as [index:4]
+print(name[0:])# it same as [0:length]
+
+print(name[1:4:2])
+
+print(len(name))#length of string   
+print(name.endswith("arr")) #last character
+print(name.startswith("h")) #first character    
