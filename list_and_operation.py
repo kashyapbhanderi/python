8d39b06e5981fc1f4416c5945e2df00fddc5b140
kashyap bhanderi <148347704+kashyapbhanderi@users.noreply.github.com>
2025-07-15 22:31:22 +0530

    Add files via upload

---
+++ list_and_operation.py
@@ -0,0 +1,18 @@
+freinds=["apple","orange",5,345.09,False,"Akasha","rohan"]
+print(freinds[3])
+print(freinds[0:3])#this an list not array
+print("\n")
+print(freinds[0])
+freinds[0]="banana" #this is mutable ,inlike string
+print(freinds[0])
+
+
+
+l1=[1,2,6,4,5]
+# l1.sort()
+# print(l1) 
+# l1.reverse()  
+l1.insert(2,33)
+print(l1)
+l1.pop(2)
+print(l1)
